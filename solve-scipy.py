3270128cd02989a7ab19dd564491beb38ac39a7a
Tom Ward <tomward@fmail.co.uk>
2023-05-29 12:15:34 +0100

    fix: correctly solve the first pair of equations

--- solve-scipy.py
+++ solve-scipy.py
@@ -1,5 +1,5 @@
 
-import scipy.optimize as opt
+from scipy.optimize import fsolve
 
 # equations:
 
@@ -8,22 +8,16 @@ import scipy.optimize as opt
 # SW (493x^2)-(125(y+.1)^2)=1
 # WN (156y^2)-(227(x+.1)^2)=1
 
-equations = [(148*(x**2))-(305*((y-.1)**2))-1, (540*(y**2))-(122*((x-.1)**2))-1, (493*(x**2))-(125*((y+.1)**2))-1, (156*(y**2))-(227*((x+.1)**2))-1]
+def equations(vars):
+    x,y = vars
+    eqs = [(148*(x**2))-(305*((y-.1)**2))-1, (540*(y**2))-(122*((x-.1)**2))-1]
+    return eqs
 
 # attempt to solve pairs of equations
 
 # first pair have a good soln at (-0.0823, 0.0968), a near soln at (0.1153, 0.0436) & two other far solns...
 
-for x in range(0, 4):
-    print(f"pair {x}")
-    equation_1 = equations[x]
-    if x < 3:
-        equation_2 = equations[x+1]
-    else:
-        equation_2 = equations[0]
-    try:
-        x,y = fsolve([equation_1, equation_2], (-0.5,0.5))
-        print((x,y))
-    except Exception:
-        print("could not solve")
+# we only want solutions in the upper-left quadrant, so seed the solver with a midpoint of that quadrant (outer corner of that quadrant may be a better choice - testing required)
+x,y = fsolve(equations, (-0.05,0.05))
+print((x,y))
 
