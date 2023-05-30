05744d9edfc546f2ef067c73e1a5565f462395e2
Tom Ward <tomward@fmail.co.uk>
2023-05-30 09:48:54 +0100

    refactor: sort out deltas.py a little for reuse

--- deltas.py
+++ deltas.py
@@ -1,8 +1,6 @@
 deltas = [80, 1, 125, 83]
-quadrants = ["north", "east", "south", "west"]
-starting_positions = [(-0.5, 0.5), (0.5, 0.5), (0.5, -0.5), (-0.5, -0.5)]
 
-def min_position(my_list):
+def _min_position(my_list):
     """docstring for min_position"""
     my_min = min(my_list)
     for i,x in enumerate(deltas):
@@ -11,8 +9,17 @@ def min_position(my_list):
             # TODO: do something more clever if there are multiple equal min positions
             return i
 
+def find_quadrant(my_list):
+    quadrants = ["north", "east", "south", "west"]
+    starting_positions = [(-0.5, 0.5), (0.5, 0.5), (0.5, -0.5), (-0.5, -0.5)]
+
+    posn = _min_position(my_list)
+    return (quadrants[posn], starting_positions[posn])
+
+quadrant, starting_position = find_quadrant(deltas)
+
 print(min(deltas))
-my_i = min_position(deltas)
+my_i = _min_position(deltas)
 print(my_i)
-print(f"{quadrants[my_i]} quadrant")
-print(f"starting position: {starting_positions[my_i]}")
+print(f"{quadrant} quadrant")
+print(f"starting position: {starting_position}")
