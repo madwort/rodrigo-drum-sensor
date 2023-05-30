41389d597dba45a1904449f72606c2306e1a65c1
Tom Ward <tomward@fmail.co.uk>
2023-05-30 11:42:40 +0100

    feat: parametrize tdoa test
    * make it easier to add more points to test

--- tests/test_tdoa.py
+++ tests/test_tdoa.py
@@ -1,10 +1,15 @@
 from drum_sensor.tdoa import calculate_point
 
 import numpy
+import pytest
 
-def test_calculate_point():
-    time_deltas_samples = [0, 80, 125, 83]
+testdata = [
+    ([0, 80, 125, 83], (-0.08199618647736903, 0.08692143585675399)),
+    ([7, 4, 0, 4], (0.004566751345874217, -0.0045667513458737))
+]
 
+@pytest.mark.parametrize("time_deltas_samples,expected_point", testdata)
+def test_calculate_point(time_deltas_samples, expected_point):
     speed = 82
 
     #  in m
@@ -12,6 +17,7 @@ def test_calculate_point():
 
     x, y = calculate_point(time_deltas_samples, speed, distance)
 
-    assert numpy.isclose(x,-0.08199618647736903)
-    assert numpy.isclose(y,0.08692143585675399)
+    expected_x, expected_y = expected_point
+    assert numpy.isclose(x,expected_x)
+    assert numpy.isclose(y,expected_y)
 
