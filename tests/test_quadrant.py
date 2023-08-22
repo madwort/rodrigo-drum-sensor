0239f18da22f29d0c586e89c63383fdc76411c2f
Tom Ward <tomward@fmail.co.uk>
2023-08-22 14:58:47 +0100

    refactor quadrant tests

--- tests/test_quadrant.py
+++ tests/test_quadrant.py
@@ -2,10 +2,15 @@ from drum_sensor import quadrant
 
 import pytest
 
-testdata = [
+conversion_testdata = [
     ([0, 80, 125, 83], [80, 45, -42, -83]),
     ([70, 117, 77, 0], [47, -40, -77, 70]),
     ([29, 30, 0, 31], [1, -30, 31, -2]),
+]
+
+quadrant_testdata = [
+    ([80, 1, 125, 83], "east", (0.0505, 0.0505)),
+    ([0, 80, 125, 83], "north", (-0.0505, 0.0505)),
 ]
 
 midpoint_testdata = [
@@ -16,6 +21,13 @@ midpoint_testdata = [
     ([0, 0, 0, 0], "dead-centre", (0, 0)),
     ([0, 0, 0, 1], "dead-centre", (0, 0)),
     ([64, 64, 1, 0], "west", (-0.0505, -0.0505)),
+]
+
+crossings_testdata = [
+    ([80, 45, -42, -83], [0]),
+    ([1, -30, 31, -2], [0, 2]),
+    ([0, -30, 0, 30], [2, 3]),
+    ([-1.5, -1.5, -1.5, -1.5], []),
 ]
 
 
@@ -37,22 +49,16 @@ def test__min_position_twin():
     assert my_i == [1, 2]
 
 
-def test_quadrant():
-    deltas = [80, 1, 125, 83]
+@pytest.mark.parametrize(
+    "time_deltas_samples,quadrant_name,starting_point", midpoint_testdata
+)
+def test_quadrant(time_deltas_samples, quadrant_name, starting_point):
+    my_quadrant, my_starting_position = quadrant.find_quadrant(
+        time_deltas_samples, 0.202
+    )
 
-    my_quadrant, starting_position = quadrant.find_quadrant(deltas, 0.202)
-
-    assert my_quadrant == "east"
-    assert starting_position == (0.0505, 0.0505)
-
-
-def test_quadrant_2():
-    deltas = [0, 80, 125, 83]
-
-    my_quadrant, starting_position = quadrant.find_quadrant(deltas, 0.202)
-
-    assert my_quadrant == "north"
-    assert starting_position == (-0.0505, 0.0505)
+    assert my_quadrant == quadrant_name
+    assert my_starting_position == starting_point
 
 
 @pytest.mark.parametrize(
@@ -79,7 +85,9 @@ def test_quadrant_crosscorrelate():
     assert starting_position == (-0.0505, 0.0505)
 
 
-@pytest.mark.parametrize("time_deltas_samples,time_deltas_samples_cross", testdata)
+@pytest.mark.parametrize(
+    "time_deltas_samples,time_deltas_samples_cross", conversion_testdata
+)
 def test_quadrant_compare(time_deltas_samples, time_deltas_samples_cross):
     my_quadrant, starting_position = quadrant.find_quadrant(time_deltas_samples, 0.202)
     my_quadrant_cross, starting_position_cross = quadrant.find_quadrant(
@@ -104,7 +112,9 @@ def test_convert_cross_samples_absolute2():
     assert absolute_deltas == [64, 64, 1, 0]
 
 
-@pytest.mark.parametrize("time_deltas_samples,time_deltas_samples_cross", testdata)
+@pytest.mark.parametrize(
+    "time_deltas_samples,time_deltas_samples_cross", conversion_testdata
+)
 def test_convert_cross_samples_absolute(time_deltas_samples, time_deltas_samples_cross):
     relative_deltas = time_deltas_samples_cross
     absolute_deltas = quadrant.convert_cross_samples_absolute(relative_deltas)
@@ -112,22 +122,7 @@ def test_convert_cross_samples_absolute(time_deltas_samples, time_deltas_samples
     assert absolute_deltas == time_deltas_samples
 
 
-def test_find_zero_crossings():
-    crossings = quadrant.find_zero_crossings([80, 45, -42, -83])
-    assert crossings == [0]
-
-
-def test_find_zero_crossings_double():
-    crossings = quadrant.find_zero_crossings([1, -30, 31, -2])
-    assert crossings == [0, 2]
-
-
-def test_find_zero_crossings_zero_1():
-    crossings = quadrant.find_zero_crossings([0, -30, 0, 30])
-    assert crossings == [2, 3]
-
-
-def test_find_zero_crossings_none():
-    # this was some actual generated test data, unclear what it means in the real world
-    crossings = quadrant.find_zero_crossings([-1.5, -1.5, -1.5, -1.5])
-    assert crossings == []
+@pytest.mark.parametrize("time_deltas,crossings", crossings_testdata)
+def test_find_zero_crossings(time_deltas, crossings):
+    my_crossings = quadrant.find_zero_crossings(time_deltas)
+    assert my_crossings == crossings
