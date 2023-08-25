4defb199f89fae24213a31a6134384a179520883
Tom Ward <tomward@fmail.co.uk>
2023-08-25 20:06:44 +0100

    add a couple of specific tests for points of interest

--- tests/test_generative.py
+++ tests/test_generative.py
@@ -4,6 +4,7 @@ from drum_sensor.tdoa import (
 from drum_sensor.samples import convert_seconds_to_samples
 import math
 import pytest
+import numpy
 
 distance = [0.202]
 
@@ -72,4 +73,40 @@ def test_calculate_point_batch(distance, test_point):
     print(f"error: {error}")
     print("-------------------------------------------")
 
+    # assert std_x < 0.0003
+    # assert std_y < 0.0003
     assert error < 0.011
+
+specific_test_points = [
+    (0.006733333333333327, -0.101),
+    # (0.006733333333333327, 0.006733333333333327),
+    (0.000100000000000003,-0.1009),
+    # (0.000100000000000003,-0.0509),
+]
+
+@pytest.mark.parametrize("distance", distance)
+@pytest.mark.parametrize("test_point", specific_test_points)
+def test_calculate_point_specific(distance, test_point):
+    speed = 82
+
+    #  in m
+    # distance = 0.202
+
+    # test_point = (0.05, 0.05)
+    time_deltas_samples = _generate_tdoa_from_point(test_point, speed, distance)
+
+    ptp = numpy.ptp(time_deltas_samples)
+    x, y, std_x, std_y = calculate_point(time_deltas_samples, speed, distance)
+
+    error = _diagonal_distance((x, y), test_point)
+
+
+    print("-------------------------------------------")
+    print(f"test point: {test_point}")
+    print(f"time deltas: {time_deltas_samples}")
+    print(f"ptp: {ptp}")
+    print(f"calculated point: {x},{y}")
+    print(f"error: {error}")
+    print("-------------------------------------------")
+
+    assert False
