2fe27e7caddf8c37abea05de86fd530d9050e596
Tom Ward <tomward@fmail.co.uk>
2023-08-29 15:21:14 +0100

    feat: add widget to analyse some of the raw data

---
+++ show_audio_output_error.py
@@ -0,0 +1,107 @@
+import json
+import matplotlib.pyplot as plt
+import numpy
+
+
+def main():
+    with open("raw-data/v2/north.json") as f:
+        d = json.load(f)
+
+        fig, axs = plt.subplots(1, 5)
+
+        x = []
+        y1 = []
+        y2 = []
+        y3 = []
+        y4 = []
+        y5 = []
+
+        synth_data = [
+            [0.0, 0.0, 0.0, 0.0],
+            [
+                10.006712282059766,
+                -10.006712282059766,
+                -11.505482840300715,
+                11.505482840300715,
+            ],
+            [
+                18.556881063290305,
+                -18.556881063290305,
+                -24.467509181430664,
+                24.467509181430664,
+            ],
+            [
+                25.76611398952776,
+                -25.76611398952776,
+                -38.77047137755371,
+                38.77047137755371,
+            ],
+            [
+                31.796318351337135,
+                -31.796318351337135,
+                -54.252462138104804,
+                54.252462138104804,
+            ],
+            [
+                36.825584940357984,
+                -36.825584940357984,
+                -70.73539067144443,
+                70.73539067144443,
+            ],
+            [
+                41.02516958487952,
+                -41.02516958487952,
+                -88.04800114928337,
+                88.04800114928337,
+            ],
+        ]
+
+        for i in range(83):
+            block = (int(i) + 1) // 12
+
+            ptp1 = numpy.ptp(d["non_filtered"]["256_nonparabolic"][f"{i}"])
+            ptp2 = numpy.ptp(d["non_filtered"]["512_nonparabolic"][f"{i}"])
+            ptp3 = numpy.ptp(d["120hp"]["512_nonparabolic"][f"{i}"])
+            ptp4 = numpy.ptp(d["500hp"]["512_nonparabolic"][f"{i}"])
+            ptp5 = numpy.ptp(synth_data[block])
+
+            print(f"{i} - {ptp1}")
+            if ((int(i) + 1) % 12) == 0:
+                print(" -- ")
+
+            x.append(block)
+            y1.append(ptp1)
+            y2.append(ptp2)
+            y3.append(ptp3)
+            y4.append(ptp4)
+            y5.append(ptp5)
+
+        axs[0].set_title("non filtered 256 non parabolic")
+        axs[0].scatter(x, y1)
+        axs[1].set_title("non filtered 512 non parabolic")
+        axs[1].scatter(x, y2)
+        axs[2].set_title("120hp 512 non parabolic")
+        axs[2].scatter(x, y3)
+        axs[3].set_title("500hp 512 non parabolic")
+        axs[3].scatter(x, y4)
+        axs[4].set_title("synthetic")
+        axs[4].scatter(x, y5)
+
+        # all_limits = [axs[0].get_ylim(), axs[1].get_ylim(), axs[2].get_ylim(), axs[3].get_ylim()]
+        # print(all_limits)
+        # print(max(all_limits))
+        # max_lim = max(all_limits)
+
+        # I happen to know this has the biggest limit
+        max_lim = axs[3].get_ylim()
+
+        axs[0].set_ylim(max_lim)
+        axs[1].set_ylim(max_lim)
+        axs[2].set_ylim(max_lim)
+        axs[3].set_ylim(max_lim)
+        axs[4].set_ylim(max_lim)
+        plt.show()
+
+
+if __name__ == "__main__":
+    main()
